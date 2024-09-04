11f8e8bc81ee1ae7a045bffefc9316ff56ba635b
TurtleSun <108293242+mlxs01@users.noreply.github.com>
2024-09-04 10:26:00 -0400

    Created Android Hello World
    Started off with displaying Hello World on Android using Kotlin

---
+++ app/src/main/java/com/example/a501assignment1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a501assignment1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a501assignment1.ui.theme._501Assignment1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _501Assignment1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _501Assignment1Theme {
+        Greeting("Android, also if here, u did not compile and run on emulator or physical android.")
+    }
+}
